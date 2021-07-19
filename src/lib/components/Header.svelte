7b8ab20c16e3d6dfbf068af5fe5d8335278236c6
oski3 <osmancanboga@hotmail.com>
2021-07-19 18:52:00 +0300

    layout created

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Anasayfa</a>
+        </li>
+        <li>
+            <a href="/about">Hakkımızda</a>
+        </li>
+        <li>
+            <a href="/contact">İletişim</a>
+        </li>
+        <li>
+            <a href="/blog">Blog</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        background-color: blueviolet;
+    }
+    li{
+        margin-right: 10px;
+    }
+    a{
+        color:black;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Header from "$lib/components/Header.svelte"
+</script>
+<div class="container">
+    <Header></Header>
+    <slot></slot>
+</div>
+
+<style>
+    .container{
+        max-width: 700px;
+        margin: 50px auto;
+     }
+</style>
